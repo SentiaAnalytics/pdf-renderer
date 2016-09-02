c27f8a6c4d8f753328d532d32c9f75e5d8e7884b
Ganaraj P R <ganarajpr@gmail.com>
2016-09-02 14:29:03 +0100

    refactoring into single module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const renderPdf = require('./html2pdf');
 const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
 
 const app = express();
 
 app.use(bodyParser.text());
 
-app.post('/pdf', renderPdf);
+app.post('/pdf', (req, res) => {
+    pdf.create(req.body).toStream(function(err, stream){
+        res.set('Content-Type', 'application/pdf');
+        res.set('Content-Disposition', 'attachment; filename=invoice.pdf');
+        stream.pipe(res);
+    });
+});
 
 module.exports = app;
--- src/html2pdf.js
+++
@@ -1,9 +0,0 @@
-const pdf = require('html-pdf');
-
-module.exports = (req, res) => {
-    pdf.create(req.body).toStream(function(err, stream){
-        res.set('Content-Type', 'application/pdf');
-        res.set('Content-Disposition', 'attachment; filename=invoice.pdf');
-        stream.pipe(res);
-    });
-};
