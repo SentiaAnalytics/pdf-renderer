903f5e5a921bf42bc531026685bde1eef33274ab
Ganaraj P R <ganarajpr@gmail.com>
2016-09-02 09:12:08 +0100

    adding pm2 config

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require('./src/app');
-const {PORT} = process.env;
+const PORT = process.env.PORT;
+
+if(!PORT) {
+  console.log('Please provide a PORT');
+  process.exit(1);
+}
 
 app.listen(PORT, () => {
   console.log(`server listening on port: ${PORT}`);
